a50a85342dfd6762101bc7d394672f93e5b989b0
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-22 23:20:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  
 const products = [
   {name: 'laptop', price: 154000},
   {name: 'mobile', price: 54000},
@@ -12,12 +15,12 @@ const products = [
 
   return (
     <div className="App">
-      {
-        products.map(product=> <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name="laptop" price="47000"></Product>
-      <Product name="phone" price="75000"></Product> */}
-    
+     {/* {
+       products.map(product=> <Product name={product.name} price={product.price}></Product>)
+       } */}
+      // {/* <Product name="laptop" price="47000"></Product>
+      // <Product name="phone" price="75000"></Product> */}
+    <ExternalUsers></ExternalUsers>
     </div>
   );
 
@@ -30,6 +33,33 @@ const products = [
     )
   }
 
-}
+  function ExternalUsers(){
+    const [users, setUsers] = useState([]);
+    useEffect( ()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+    }, [])
+    return (
+      <div>
+        <h2>External Users</h2>
+        <p>{users.length}</p>
+        {
+          users.map(user => <User name={user.name} email={user.email}></User>)
+        }
+      </div>
+    )
+     
+  function User(props){
+    return (
+      <div>
+        <h3>Name: {props.name}</h3>
+        <p>Email: {props.email}</p>
+      </div>
+    )
+  }
 
+}
+}
 export default App;
+
